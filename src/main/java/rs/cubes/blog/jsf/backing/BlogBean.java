079ab7658623fb8a7af5348ec3fcf6b7be679456
Cvetanovic <cvetanovic.nikola@gmail.com>
2019-05-13 20:31:22 +0200

    dodat register.xhtml

--- src/main/java/rs/cubes/blog/jsf/backing/BlogBean.java
+++ src/main/java/rs/cubes/blog/jsf/backing/BlogBean.java
@@ -6,6 +6,7 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import rs.cubes.blog.domain.User;
 import rs.cubes.blog.service.UserService;
 
 @Named
@@ -21,5 +22,68 @@ public class BlogBean implements Serializable{
 	private String name;
 	private String surname;
 	private String nickname;
+	
+	
+	public void createUser(String username, String password, String email, String name, String surname, String nickname) {
+		User u = new User();
+		u.setUsername(username); 
+		u.setPassword(password);
+		u.setEmail(email);
+		u.setName(nickname);
+		u.setSurname(surname);
+		u.setNickname(nickname);
+		userService.createUser(u);
+		
+	}
+	
+	
+	
+	
+	
+	public UserService getUserService() {
+		return userService;
+	}
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
+	
 
 }
